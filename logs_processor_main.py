97949615175de30b22d8a398c598758934fe0184
Sufian Mushtaq <sufian@Sufians-Laptop.local>
2024-04-11 16:43:29 -0400

    Refactor logic to populate dictionary

--- logs_processor_main.py
+++ logs_processor_main.py
@@ -27,34 +27,30 @@ def main():
         print(e)
         sys.exit(-1)
 
-    output_avg = dict()
-    output_max = dict()
+    avg_rows = dict()
+    max_rows = dict()
     # Perform computations on each file
     for file_path in files:
         print(f"COMPUTING FOR: {file_path}")
         file_avg, file_max = compute(file_path)
-        output_avg[file_path] = file_avg
-        output_max[file_path] = file_max
+        avg_rows[file_path] = file_avg
+        max_rows[file_path] = file_max
 
     # output the result
-    io.output_to_csv(output_dir, output_avg, "file1.csv")
-    io.output_to_csv(output_dir, output_max, "file2.csv")
+    io.output_to_csv(output_dir, avg_rows, "file1.csv")
+    io.output_to_csv(output_dir, max_rows, "file2.csv")
 
 
 """
     file_path: the path to the hdf5 file
-    output_avg: the output dictionary for avg positions
-    output_max: the output dictionary for max position 
+    avg_rows: the output dictionary for avg positions
+    max_rows: the output dictionary for max position 
     return: a list with average positions for each sensor for a device
 """
 
 
 def compute(file_path: str):
-    output_avg = dict()
-    output_max = dict()
-    # open file
     with h5py.File(file_path, "r") as file:
-        # check if the hdf5 is formatted correctly and calculate average position
         for device, obj in file.items():
             # verify the structure of the hdf5 file
             try:
@@ -66,27 +62,23 @@ def compute(file_path: str):
 
             # compute
             print(f"Device: {device}")
-
             dataset = np.array(file[device]["Position"])
+
             # get the average
             avg_list = lp.compute_avg(dataset)
             # get the maximum distance
             max_list = lp.compute_max(dataset)
 
-            # TODO: Make this into a helper method
             # create output rows
-            output_avg["file"] = file_path
-            output_max["file"] = file_path
-
-            for i, lis in enumerate(avg_list):
-                output_avg[f"{device}_S{i}_x"] = lis[0]
-                output_avg[f"{device}_S{i}_y"] = lis[1]
-                output_avg[f"{device}_S{i}_z"] = lis[2]
+            avg_rows = lp.get_avg_rows(avg_list, device)
+            avg_rows["file"] = file_path
+            max_rows = lp.get_max_rows(max_list, device)
+            max_rows["file"] = file_path
 
             for i, val in enumerate(max_list):
-                output_max[f"{device}_S{i}_max"] = val
+                max_rows[f"{device}_S{i}_max"] = val
 
-    return output_avg, output_max
+    return avg_rows, max_rows
 
 
 if __name__ == "__main__":
--- util/processing/logs_processing.py
+++ util/processing/logs_processing.py
@@ -13,3 +13,21 @@ def compute_max(dataset):
     Returns a numpy array with max
     """
     return np.max(np.linalg.norm(dataset, axis=2), axis=0)
+
+
+def get_avg_rows(dataset, device_name) -> dict:
+    avg_rows = dict()
+    for sensor_index, value in enumerate(dataset):
+        avg_rows[f"{device_name}_S{sensor_index}_x"] = value[0]
+        avg_rows[f"{device_name}_S{sensor_index}_y"] = value[1]
+        avg_rows[f"{device_name}_S{sensor_index}_z"] = value[2]
+
+    return avg_rows
+
+
+def get_max_rows(max_list, device_name) -> dict:
+    max_rows = dict()
+    for sensor_index, value in enumerate(max_list):
+        max_rows[f"{device_name}_S{sensor_index}_max"] = value
+
+    return max_rows
